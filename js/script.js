cdc1985f6eaf8ac2ccf8d94c11d8e7cc918fc358
Kirtiman-Senapati <kirtimansenapati29@gmail.com>
2023-01-06 23:34:43 +0530

    Responsive  for mobile view

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@ let SearchForm = document.querySelector('.search-form'); // create variable and 
 document.querySelector('#search-btn').onclick = () =>    // after click icon it works active
 {
     SearchForm.classList.toggle('active');
+    shopping.classList.remove('active');
+    loginform.classList.remove('active');
+    navbar.classList.remove('active');
+
 }
 
 
@@ -12,6 +16,9 @@ let shopping = document.querySelector('.shopping-cart'); // create variable and 
 document.querySelector('#cart-btn').onclick = () =>    // after click icon it works active
 {
     shopping.classList.toggle('active');
+    SearchForm.classList.remove('active');
+    loginform.classList.remove('active');
+    navbar.classList.remove('active');
 }
 
 
@@ -21,4 +28,33 @@ let loginform = document.querySelector('.login-form'); // create variable and se
 document.querySelector('#login-btn').onclick = () =>    // after click icon it works active
 {
     loginform.classList.toggle('active');
+    SearchForm.classList.remove('active');
+    shopping.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+
+
+// clickable for 768 px/ mobile
+
+let navbar = document.querySelector('.navbar'); // create variable and select the quries 1st /select form 1st 
+
+document.querySelector('#menu-btn').onclick = () =>    // after click icon it works active
+{
+    navbar.classList.toggle('active');
+    SearchForm.classList.remove('active');
+    shopping.classList.remove('active');
+    loginform.classList.remove('active');
+    
+}
+
+// remove for mobile
+
+window.onscroll = () =>    // after click icon it works active
+{
+    SearchForm.classList.remove('active');
+    shopping.classList.remove('active');
+    loginform.classList.remove('active');
+    navbar.classList.remove('active');
+
 }
