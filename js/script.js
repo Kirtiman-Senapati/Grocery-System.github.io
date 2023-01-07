05089acd26f8decfc189fd3fcb50301b54937c94
Kirtiman-Senapati <kirtimansenapati29@gmail.com>
2023-01-07 23:29:54 +0530

    New things add  product Section

--- js/script.js
+++ js/script.js
@@ -61,28 +61,24 @@ window.onscroll = () =>    // after click icon it works active
 
 
 
-var swiper = new Swiper(".product-slider", 
-{
-    loop:true,
-    spaceBetween: 20,
-    autoplay:
-    {
-      delay:7500,
-      disableOnInteraction:false,
-    }, 
-    breakpoints: 
-    {
-      0: 
-      {
-        slidesPerView: 1,
-      },
-      768: 
-      {
-        slidesPerView: 2,
-      },
-      1020: 
-      {
-        slidesPerView: 3,
-      },
+var swiper = new Swiper(".product-slider", {
+  loop:true,
+  spaceBetween: 20,
+
+  autoplay:{
+    delay:7500,
+    disableOnInteraction:false,
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
 });
